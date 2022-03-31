4242dba10203e5897315c983644487907a7fa5ab
Cuber-final <achilescuber@gmail.com>
2022-03-31 22:41:01 +0800

    add some new api

--- wx/urls.py
+++ wx/urls.py
@@ -4,12 +4,13 @@ from wx.views import *
 app_name = 'api'
 
 urlpatterns = [
-    path('login', UserLogin.as_view(), name='login'),
-    path('register', Register.as_view(), name='register'),
+    path('user/login', UserLogin.as_view(), name='login'),
+    path('user/register', Register.as_view(), name='register'),
     path('search/camera',CameraRec.as_view(),name='camera'),
     path('search/getCates', GetCates.as_view(), name='getCates'),
     path('search/text',TextSearch.as_view(),name='text'),
     path('search/addCate',AddCate.as_view(),name='addCate'),
+    path('user/history',GetHistory.as_view(),name='getHistory'),
     path('', theIndex.as_view(), name='index'),
     path('test', UtilApi.as_view(), name='show')
 
--- wx/views.py
+++ wx/views.py
@@ -67,44 +67,78 @@ class TextSearch(APIView):
             return Response("the cate doesn't exist")
         parent_cate = TcCate.objects.get(tc_id=cate.tc_parent_id)
         par_name = parent_cate.tc_name
-        fb_dic = {"name": cate.tc_name, "parent_name": par_name}
+        fb_dic = {"name": cate.tc_name, "pc_name": par_name}
         return Response(fb_dic)
 
     def post(self, request):
-        cate_name = request.data["name"]
-        cate = TcCate()
-        try:
-            cate = TcCate.objects.get(tc_name=cate_name)
-        except cate.DoesNotExist:
-            return Response("the cate doesn't exist")
-        parent_cate = TcCate.objects.get(tc_id=cate.tc_parent_id)
-        par_name = parent_cate.tc_name
-        fb_dic = {"name": cate.tc_name, "parent_name": par_name}
-        return Response(fb_dic)
+        """
+        :param request: 包括用户名，垃圾名称/搜索内容,搜索方式为文本搜索
+        :return: 返回查询的垃圾所属类别,暂时只保存查询成功的信息
+        应当支持模糊搜索？
+        """
+        # 分为两种情景模式，游客与用户
+        use_status = request.data['stat']
+        sea_info = request.data["content"]
+        if use_status == 'visitor':
+            cate = TcCate()
+            # 先检测是否存在当前垃圾的数据，没有则直接返回失败
+            try:
+                cate = TcCate.objects.get(tc_name=sea_info)
+            except cate.DoesNotExist:
+                return Response({'msg': '未找到任何信息', 'code': 0})
+            cateName = cate.tc_name
+            parent_cate = TcCate.objects.get(tc_id=cate.tc_parent_id)
+            par_name = parent_cate.tc_name
+            fb_msg = {'name': cateName, 'pc_name': par_name, 'msg': '查找结果如下', 'code': 1}
+            return Response(fb_msg)
+        else:
+            username = request.data['username']
+            user = TcUser.objects.get(username=username)
+            userId = user.userId
+            cate = TcCate()
+            fb_msg = {'msg': '当前信息曾被查询过，可通过历史记录查看', 'code': 0}
+            # 先检测是否存在当前垃圾的数据，没有则直接返回失败
+            try:
+                cate = TcCate.objects.get(tc_name=sea_info)
+            except cate.DoesNotExist:
+                return Response({'msg': '未找到任何信息', 'code': 0})
+            # 若库内没有当前用户下与这个词条相关的记录，则可以添加搜索的记录
+            history = TcSearch()
+            try:
+                history = TcSearch.objects.get(sea_info=sea_info, sea_user_id=userId)
+            except history.DoesNotExist:
+                cateId = cate.tc_id
+                cateName = cate.tc_name
+                parent_cate = TcCate.objects.get(tc_id=cate.tc_parent_id)
+                par_name = parent_cate.tc_name
+                TcSearch.objects.create(sea_info=sea_info, sea_way=0, sea_user_id=userId,
+                                        sea_cate_id=cateId)
+                fb_msg = {'name': cateName, 'pc_name': par_name, 'msg': '查找成功，结果如下', 'code': 1}
+            return Response(fb_msg)
 
 
 # 获取分类信息
 class GetCates(APIView):
     def get(self, request):
-        cate_dict = {}
+        # 获取所有一级分类,将每一个数据打包成字典列表
+        cate_set = []
         cates = TcCate.objects.all()
         for cate in cates:
-            cate_name = cate.tc_name
-            cate_id = cate.tc_id
-            cate_dict[cate_id] = cate_name
-        # json_dict = json.dumps(cate_dict, ensure_ascii=False) 不需要解析为json字符串
-        return Response(cate_dict)
+            if cate.tc_parent_id == cate.tc_id:
+                cate_dict = {"tcId": cate.tc_id, "tcName": cate.tc_name}
+                cate_set.append(cate_dict)
+                # json_dict = json.dumps(cate_dict, ensure_ascii=False) 不需要解析为json字符串
+        return Response(cate_set)
 
     # 查询某一父类下的所有垃圾种类
     def post(self, request):
-        cate_dict = {}
-        parent_id = request.data["id"]
+        cate_set = []
+        parent_id = request.data["tcId"]
         cates = TcCate.objects.filter(tc_parent_id=parent_id).exclude(tc_id=parent_id)
         for cate in cates:
-            cate_name = cate.tc_name
-            cate_id = cate.tc_id
-            cate_dict[cate_id] = cate_name
-        return Response(cate_dict)
+            cate_dict = {"tcId": cate.tc_id, "tcName": cate.tc_name}
+            cate_set.append(cate_dict)
+        return Response(cate_set)
 
 
 # 添加分类信息
@@ -124,6 +158,38 @@ class AddCate(APIView):
             TcCate.objects.create(tc_name=cate_name, tc_parent_id=parent_id)
         msg = fb_msg[fb_code]
         return Response({'fb_msg': msg})
+
+
+# 查询当前用户的所有历史搜索记录
+class GetHistory(APIView):
+    def get(self, request):
+        # username=request.data['username']
+        username = 'userB'
+        userId = TcUser.objects.get(username=username).userId
+        sea_list = []
+        sea_dict = {}
+        infos = TcSearch.objects.filter(sea_user_id=userId)
+        for info in infos:
+            pid = TcCate.objects.get(tc_id=info.sea_cate_id).tc_parent_id
+            pcName = TcCate.objects.get(tc_id=pid).tc_name
+            sea_dict['seaInfo'] = info.sea_info
+            sea_dict['pcName'] = pcName
+            sea_list.append(sea_dict)
+        return Response(sea_list)
+
+    def post(self, request):
+        username = request.data['username']
+        userId = TcUser.objects.get(username=username).userId
+        sea_list = []
+        sea_dict = {}
+        infos = TcSearch.objects.filter(sea_user_id=userId)
+        for info in infos:
+            pid = TcCate.objects.get(tc_id=info.sea_cate_id).tc_parent_id
+            pcName = TcCate.objects.get(tc_id=pid).tc_name
+            sea_dict['seaInfo'] = info.sea_info
+            sea_dict['pcName'] = pcName
+            sea_list.append(sea_dict)
+        return Response(sea_list)
 
 
 '''
